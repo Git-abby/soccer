13c24d0f1bb82009f9ddd40ae7bd114b9b69281d
Git-abby <malekabrar56@gmail.com>
2024-05-19 18:00:43 -0400

    Backend CRUD Operation finished

---
+++ backend/controllers/playerControllers.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+import { PlayerSchema } from "../models/playerModel";
+
+const Player = mongoose.model("Player1", PlayerSchema);
+
+export const addNewPlayer = async (req, res) => {
+  let newPlayer = new Player(req.body);
+
+  try {
+    const player = await newPlayer.save();
+    res.json(player);
+  } catch (err) {
+    res.send(err);
+  }
+};
+
+export const getAllPlayers = async (req, res) => {
+  try {
+    const players = await Player.find({});
+    res.json(players);
+  } catch (err) {
+    res.send(err);
+  }
+};
+
+export const getPlayerWithId = async (req, res) => {
+  try {
+    const player = await Player.findById(req.params.PlayerId);
+    res.json(player);
+  } catch (err) {
+    res.send(err);
+  }
+};
+
+export const updatePlayerWithId = async (req, res) => {
+  try {
+    const player = await Player.findByIdAndUpdate(
+      {
+        _id: req.params.PlayerId,
+      },
+      req.body,
+      { new: true }
+    );
+    res.json(player);
+  } catch (err) {
+    res.send(err);
+  }
+};
