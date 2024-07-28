b261bbaffd3e30da1f849fbd8a982e137de923e2
Git-abby <malekabrar56@gmail.com>
2024-07-28 12:06:49 -0400

    Logout done

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,6 +4,10 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 export const UserSchema = new Schema({
+  name: {
+    type: String,
+    require: true,
+  },
   email: {
     type: String,
     require: true,
